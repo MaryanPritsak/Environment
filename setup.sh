40ef69c909126aecf413f6d78df3d82e7dae0733
Marian Pritsak <maryan.pricak@gmail.com>
2017-05-02 18:09:40 +0300

    [setup.sh]: Initial support for git.
    Install git.
    Update git aliases.

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+set -x
+
+#
+# Git
+#
+
+sudo apt-get install -y git
+
+git config --global alias.br 'branch'
+git config --global alias.ci 'commit'
+git config --global alias.co 'checkout'
+git config --global alias.gr 'grep -n'
+git config --global alias.st 'status'
+git config --global alias.subdate 'submodule update --init --recursive'
+
+
