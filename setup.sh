e8ba5153650a36afb5fd34823929bba65ba91ac7
Marian Pritsak <maryan.pricak@gmail.com>
2017-07-30 13:28:26 +0300

    [setup.sh][vim]: Add plugins
    Signed-off-by: Marian Pritsak <maryan.pricak@gmail.com>

--- setup.sh
+++ setup.sh
@@ -73,6 +73,12 @@ call vundle#begin()
 " let Vundle manage Vundle, required
 Plugin 'VundleVim/Vundle.vim'
 
+Plugin 'easymotion/vim-easymotion'
+
+Plugin 'crusoexia/vim-monokai'
+
+Plugin 'ervandew/supertab'
+
 " All of your Plugins must be added before the following line
 call vundle#end()            " required
 filetype plugin indent on    " required
